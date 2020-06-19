91c62158729a04d2eea96f4b1a593de9a8ccd61f
Austin Barrett <virus.cmd@gmail.com>
2020-06-18 22:08:08 -0400

    nav panel loop fix

--- pages/index.vue
+++ pages/index.vue
@@ -32,7 +32,7 @@
               </span>
             </p>
           </div>
-          <n-link class="panel-block is-active" v-for="(key, link) in links" :to="link.path" :key="key">
+          <n-link class="panel-block is-active" v-for="(link, key) in links" :to="link.path" :key="key">
             {{link.name | name}}
           </n-link>
         </nav>
